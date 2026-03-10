810a10ec74962a6af6ac71620a9b1fcb9095b38a
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-03-10 13:21:46 +0700

    Update

--- BaiTH2_1/app/src/main/java/com/example/bt2_1/MainActivity.java
+++ BaiTH2_1/app/src/main/java/com/example/bt2_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bt2_1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -20,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XuLiCong(View view){
+        EditText editTextView = findViewById(R.id.edtA);
+        EditText editTextView1 = findViewById(R.id.edtB);
+        EditText editTextView2 = findViewById(R.id.edtKetqua);
+
+        String strA =  editTextView.getText().toString();
+        String strB =  editTextView1.getText().toString();
+
+        int soA = Integer.parseInt(strA);
+        int soB = Integer.parseInt(strB);
+
+        int tong = soA + soB;
+        String strTong = String.valueOf(tong);
+        editTextView2.setText(strTong);
+
     }
 }
