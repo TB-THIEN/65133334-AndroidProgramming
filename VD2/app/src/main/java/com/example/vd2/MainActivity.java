3912c8c2629b508f0afe160d5f427eed08ee6612
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-03-11 13:08:26 +0700

    update

--- VD2/app/src/main/java/com/example/vd2/MainActivity.java
+++ VD2/app/src/main/java/com/example/vd2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.vd2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //KHAI BÁO CÁC ĐỐI TƯỢNG
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKq;
+    Button nutcong, nuttru, nutnhan, nutchia;
+
+    void TIMDIEUKIEN(){
+        editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        editTextKq = (EditText)findViewById(R.id.edtKq);
+
+        nutcong = (Button) findViewById(R.id.btnCong);
+        nuttru = (Button) findViewById(R.id.btnTru);
+        nutnhan = (Button) findViewById(R.id.btnNhan);
+        nutchia = (Button) findViewById(R.id.btnChia);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,76 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TIMDIEUKIEN();
+    }
+
+
+    public void XuLiCong(View v){
+
+
+        //LẤY DỮ LIỆU
+        String Sothu1 = editTextSo1.getText().toString();
+        String Sothu2 = editTextSo2.getText().toString();
+
+        //CHUYỂN DỮ LIỆU SANG DẠNG CHUỖI
+        Float soA = Float.parseFloat(Sothu1);
+        Float soB = Float.parseFloat(Sothu2);
+
+        Float tong = soA + soB;
+
+        //TÌM KẾT QUẢ
+
+        //CHUYỂN SANG DẠNG CHUỖI
+        String chuoiKQ = String.valueOf(tong);
+        //GÁN KẾT QUẢ LÊN ĐIỀU KHIỂN
+        editTextKq.setText(chuoiKQ);
+    }
+    public void XuLiTru(View v){
+        //LẤY DỮ LIỆU
+        String Sothu1 = editTextSo1.getText().toString();
+        String Sothu2 = editTextSo2.getText().toString();
+
+        //CHUYỂN DỮ LIỆU SANG DẠNG CHUỖI
+        Float soA = Float.parseFloat(Sothu1);
+        Float soB = Float.parseFloat(Sothu2);
+
+        Float hieu = soA - soB;
+
+        //CHUYỂN SANG DẠNG CHUỖI
+        String chuoiKQ = String.valueOf(hieu);
+        //GÁN KẾT QUẢ LÊN ĐIỀU KHIỂN
+        editTextKq.setText(chuoiKQ);
+    }
+    public void XuLiNhan(View v){
+        //LẤY DỮ LIỆU
+        String Sothu1 = editTextSo1.getText().toString();
+        String Sothu2 = editTextSo2.getText().toString();
+
+        //CHUYỂN DỮ LIỆU SANG DẠNG CHUỖI
+        Float soA = Float.parseFloat(Sothu1);
+        Float soB = Float.parseFloat(Sothu2);
+
+        Float tich = soA * soB;
+
+        //CHUYỂN SANG DẠNG CHUỖI
+        String chuoiKQ = String.valueOf(tich);
+        //GÁN KẾT QUẢ LÊN ĐIỀU KHIỂN
+        editTextKq.setText(chuoiKQ);
+    }
+    public void XuLiChia(View v){
+        //LẤY DỮ LIỆU
+        String Sothu1 = editTextSo1.getText().toString();
+        String Sothu2 = editTextSo2.getText().toString();
+
+        //CHUYỂN DỮ LIỆU SANG DẠNG CHUỖI
+        Float soA = Float.parseFloat(Sothu1);
+        Float soB = Float.parseFloat(Sothu2);
+
+        Float thuong = soA/soB;
+
+        //CHUYỂN SANG DẠNG CHUỖI
+        String chuoiKQ = String.valueOf(thuong);
+        //GÁN KẾT QUẢ LÊN ĐIỀU KHIỂN
+        editTextKq.setText(chuoiKQ);
     }
 }
