99737aebea8bd4f690bc9f2e75d7e6bde780efe0
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-03-27 11:14:45 +0700

    BTH9

---
+++ BaiTH9_Recyclerview/app/src/main/java/com/tbt65133334/baith9_recyclerview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.tbt65133334.baith9_recyclerview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandScape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        recyclerViewDatas = getDataForRecyclerView();
+        recyclerViewLandScape = findViewById(R.id.recyclerLand);
+        //RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        //recyclerViewLandScape.setLayoutManager(layoutLinear);
+        RecyclerView.LayoutManager layoutLinearHorizonal = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewLandScape.setLayoutManager(layoutLinearHorizonal);
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("thapchamnt", "Tháp chàm Nha Trang");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("bigbenlondon","Tháp Bigben London"));
+        return dsDuLieu;
+    }
+}
