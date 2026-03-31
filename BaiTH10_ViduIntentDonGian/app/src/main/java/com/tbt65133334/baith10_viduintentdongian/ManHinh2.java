c007cb5554f86d550dcc42fece7d21ccf7aa041b
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-03-31 20:02:47 +0700

    BTH10

---
+++ BaiTH10_ViduIntentDonGian/app/src/main/java/com/tbt65133334/baith10_viduintentdongian/ManHinh2.java
@@ -0,0 +1,30 @@
+package com.tbt65133334.baith10_viduintentdongian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ManHinh2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_man_hinh2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void QuayVe(View v){
+        Intent iManHinh2 = new Intent(this, MainActivity.class);
+        startActivity(iManHinh2);
+    }
+}
