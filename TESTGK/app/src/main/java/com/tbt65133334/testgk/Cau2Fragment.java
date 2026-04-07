a86468fa4a63b528ae8fb32828e387a893d03722
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-04-07 15:55:49 +0700

    update

--- TESTGK/app/src/main/java/com/tbt65133334/testgk/Cau2Fragment.java
+++ TESTGK/app/src/main/java/com/tbt65133334/testgk/Cau2Fragment.java
@@ -1,64 +1,57 @@
 package com.tbt65133334.testgk;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cau2Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class Cau2Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandScape;
 
     public Cau2Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cau2Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Cau2Fragment newInstance(String param1, String param2) {
-        Cau2Fragment fragment = new Cau2Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau2, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_cau2, container, false);
+
+        recyclerViewDatas = getDataForRecyclerView();
+
+        recyclerViewLandScape = view.findViewById(R.id.ryCau2);
+
+        RecyclerView.LayoutManager layoutLinearHorizonal = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewLandScape.setLayoutManager(layoutLinearHorizonal);
+
+        landScapeAdapter = new LandScapeAdapter(getContext(), recyclerViewDatas);
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+
+        return view;
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("thapchamnt", "Tháp chàm Nha Trang");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("bigbenlondon","Tháp Bigben London"));
+        return dsDuLieu;
     }
 }
