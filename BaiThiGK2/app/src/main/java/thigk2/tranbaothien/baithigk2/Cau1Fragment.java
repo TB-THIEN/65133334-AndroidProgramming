d91751a2b6afa015599d207906f3004220735352
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-04-20 07:41:35 +0700

    Lần thứ 1

---
+++ BaiThiGK2/app/src/main/java/thigk2/tranbaothien/baithigk2/Cau1Fragment.java
@@ -0,0 +1,35 @@
+package thigk2.tranbaothien.baithigk2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Cau1Fragment extends Fragment {
+
+    public Cau1Fragment() {
+        // Required empty public constructor
+    }
+
+    public static Cau1Fragment newInstance(String param1, String param2) {
+        Cau1Fragment fragment = new Cau1Fragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cau1, container, false);
+    }
+}
---
+++ BaiThiGK2/app/src/main/java/thigk2/tranbaothien/baithigk2/DanhNhan.java
@@ -0,0 +1,27 @@
+package thigk2.tranbaothien.baithigk2;
+
+public class DanhNhan {
+    String DanhNhanImageFileName;
+    String DanhNhanCation;
+
+    public DanhNhan(String landImageFileName, String landCation) {
+        this.landImageFileName = landImageFileName;
+        this.landCation = landCation;
+    }
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
+}
