c3f3aebef079150987dc936809da2facbb621202
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-04-20 08:30:18 +0700

    Lan cuoi

--- BaiThiGK2/app/src/main/java/thigk2/tranbaothien/baithigk2/Cau2Fragment.java
+++ BaiThiGK2/app/src/main/java/thigk2/tranbaothien/baithigk2/Cau2Fragment.java
@@ -1,64 +1,61 @@
 package thigk2.tranbaothien.baithigk2;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cau2Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Cau2Fragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class BaiHatAdapter extends RecyclerView.Adapter<BaiHatAdapter.BaiHatViewHolder> {
 
-    public Cau2Fragment() {
-        // Required empty public constructor
+    Context context;
+    ArrayList<BaiHat> listBaiHat;
+
+    public BaiHatAdapter(Context context, ArrayList<BaiHat> listBaiHat) {
+        this.context = context;
+        this.listBaiHat = listBaiHat;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cau2Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Cau2Fragment newInstance(String param1, String param2) {
-        Cau2Fragment fragment = new Cau2Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @NonNull
+    @Override
+    public BaiHatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_baihat, parent, false);
+        return new BaiHatViewHolder(view);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onBindViewHolder(@NonNull BaiHatViewHolder holder, int position) {
+        BaiHat baiHat = listBaiHat.get(position);
+        holder.tvTenBaiHat.setText(baiHat.getTenBaiHat());
+        holder.tvCaSi.setText(baiHat.getCaSi());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listBaiHat.size();
+    }
+
+    class BaiHatViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvTenBaiHat, tvCaSi;
+
+        public BaiHatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTenBaiHat = itemView.findViewById(R.id.tvTenBaiHat);
+            tvCaSi = itemView.findViewById(R.id.tvCaSi);
+            itemView.setOnClickListener(this);
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau2, container, false);
+        @Override
+        public void onClick(View v) {
+            int vitri = getAbsoluteAdapterPosition();
+            BaiHat baiHatDuocChon = listBaiHat.get(vitri);
+            Toast.makeText(context, "Đang phát bài: " + baiHatDuocChon.getTenBaiHat(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
